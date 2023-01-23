013a938ceedfcb3b03161d630510c52efba8b3f1
liuxiaolei001 <7711242+liuxiaolei001@user.noreply.gitee.com>
2023-01-23 21:43:57 +0800

    34_闭包应用

---
+++ 34_闭包应用/myModule2.js
@@ -0,0 +1,19 @@
+(function myModule2(window) {
+    // 私有数据
+    var msg = 'My bear'
+    // 操作数据的函数
+    function biandaxie() {
+
+        console.log('变大写'+msg.toUpperCase())
+    }
+    function bianxiaoxie() {
+        console.log('变小写'+msg.toLowerCase())
+    }
+    // 向外暴露对象,即给外部使用的方法
+    // return biandaxie
+    window.myModule2 = {
+        biandaxie:biandaxie,
+        bianxiaoxie:bianxiaoxie
+
+    }
+})(window)
